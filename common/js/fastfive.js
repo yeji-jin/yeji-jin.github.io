c396f02675b5a6f56bb8647f6d3cad8ef8dd8cb6
yeji <dpwl322@naver.com>
2024-07-25 22:50:34 +0900

    [포트폴리오] 작업이미지 추가

---
+++ common/js/fastfive.js
@@ -0,0 +1,75 @@
+const btnPrev = document.querySelector(".viewer-btns .prev");
+const btnNext = document.querySelector(".viewer-btns .next");
+const browser_PC = document.querySelector(".browser.pc");
+const browser_MB = document.querySelector(".browser.mb");
+const img_PC = browser_PC.querySelector("img");
+const img_MB = browser_MB.querySelector("img");
+let viewIndex = 0;
+const works = [
+  {
+    pc: "spot",
+    mb: "spot_m",
+  },
+  {
+    pc: "space",
+    mb: "space_m",
+  },
+  {
+    pc: "office",
+    mb: "office_m",
+  },
+  {
+    pc: "benefit",
+    mb: "benefit_m",
+  },
+  {
+    pc: "blog",
+    mb: "blog_m",
+  },
+  {
+    pc: "broker",
+    mb: "broker_m",
+  },
+  {
+    pc: "event",
+    mb: "event_m",
+  },
+];
+const setting_IMG = (idx = 0) => {
+  img_PC.src = `./common/images/fastfive/${works[idx].pc}.jpg`;
+  img_MB.src = `./common/images/fastfive//${works[idx].mb}.jpg`;
+};
+const updateButtons = () => {
+  if (viewIndex === 0) {
+    btnPrev.setAttribute("disabled", true);
+  } else {
+    btnPrev.removeAttribute("disabled");
+  }
+  if (viewIndex === works.length - 1) {
+    btnNext.setAttribute("disabled", true);
+  } else {
+    btnNext.removeAttribute("disabled");
+  }
+  //   browser_PC.scrollTop = 0;
+  //   browser_MB.scrollTop = 0;
+};
+//init
+setting_IMG();
+updateButtons();
+
+btnPrev.addEventListener("click", () => {
+  if (viewIndex > 0) {
+    --viewIndex;
+    console.log(viewIndex);
+    setting_IMG(viewIndex);
+  }
+  updateButtons();
+});
+btnNext.addEventListener("click", () => {
+  if (viewIndex < works.length - 1) {
+    ++viewIndex;
+    console.log(viewIndex);
+    setting_IMG(viewIndex);
+  }
+  updateButtons();
+});
